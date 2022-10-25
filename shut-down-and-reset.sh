ce148ddfad8df448cebe74271dca7988b412aef6
Nick Rameau <hello@nick.ht>
2022-10-25 19:43:31 -0400

    Update docker compose commands

--- shut-down-and-reset.sh
+++ shut-down-and-reset.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker-compose down --remove-orphans
+docker compose down --remove-orphans
 cd external_tools/
-docker-compose down
+docker compose down
 cd ..
 docker volume rm external_tools_cassandra-volume
 docker volume rm external_tools_postgres-volume
--- start-up.sh
+++ start-up.sh
@@ -3,7 +3,7 @@ set -e
 
 # Start up Eureka, ActiveMQ, Cassandra and Postgres
 cd external_tools/
-docker-compose up -d
+docker compose up -d
 cassandra_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cassandra)
 postgres_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgres)
 
@@ -23,12 +23,12 @@ java -cp external_tools/lang-0.1.0-BUILD-SNAPSHOT.jar org.apache.fineract.cn.lan
 cat env_variables >> .env
 
 echo "Starting Provisioner... "
-docker-compose up -d provisioner-ms
+docker compose up -d provisioner-ms
 # Make sure provisioner is up and running
 echo "Waiting for provisioner to initialize database... "
 while ! docker logs fineract-cn-docker-compose_provisioner-ms_1 | grep -q "Started ProvisionerApplication in"; do
   sleep 1
 done
 echo "Start remaining Fineract CN microservices... "
-docker-compose up -d
+docker compose up -d
 echo "Successfully started fineract services."
